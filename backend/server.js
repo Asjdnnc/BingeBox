96c3630e9d3ed2b33089a75d2ce3e26d2562b37d
unknown <you@example.com>
2025-01-26 15:18:16 +0530

    add

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
+
+import { ENV_VARS } from "./config/envVar.js";
+import connectDB from "./config/db.js";
+
+const app = express();
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/movie", movieRoutes);
+app.use("/api/tv", tvRoutes);
+app.use("/api/search", searchRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Server started on port 5000");
+  connectDB();
+});
