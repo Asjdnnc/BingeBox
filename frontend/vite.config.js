aac15eae4db5aa044e2ae11ae6eea55a5cd937a1
unknown <you@example.com>
2025-01-26 16:14:40 +0530

    change the target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://binge-20yyxg03s-solo-kings-projects.vercel.app",
 			},
 		},
 	},
